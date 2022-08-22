295ef239a5c543a4f49576d5a2df10cbb2cb2dba
rarothschild <rarothsc@gmail.com>
2022-08-22 13:16:32 -0700

    moving images

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://rarothschild.github.io"
+    site: "https://rarothschild.github.io",
+    base: "/RobSite2"
 });

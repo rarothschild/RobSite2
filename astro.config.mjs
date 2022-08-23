b0415da10b71b457d2992db37fe360266f964286
rarothschild <rarothsc@gmail.com>
2022-08-23 10:34:28 -0700

    figger it out

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: "https://rarothschild.github.io",
-    base: "/RobSite2"
+    base: "/RobSite2/"
 });

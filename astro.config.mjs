f913ab9f33e1c265d4fe890d6d82aa942c1d97b9
rarothschild <rarothsc@gmail.com>
2022-11-14 08:41:10 -0800

    Refactoring and re-stylizing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://rarothschild.github.io",
-    base: "RobSite2/",
-    trailingSlash: "never"
+  site: "https://rarothschild.github.io",
+  integrations: [tailwind(), image()]
 });
